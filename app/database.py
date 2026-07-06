2c39225597e1455644b319eb49d0ea738cebb3bf
nomanpathan485 <nomanp210405@gmail.com>
2026-07-06 23:09:03 +0530

    create room and sqlite database

---
+++ app/database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = "sqlite:///./hotel.db"
+ENGINE = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=ENGINE)
+Base = declarative_base()
---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class Room(Base):
+    __tablename__ = "rooms"
+
+    id = Column(Integer, primary_key=True, index=True)
+    supplier_code = Column(Integer)
+    supplier_name = Column(String)
+    supplier_room_name = Column(String)
+
+    
